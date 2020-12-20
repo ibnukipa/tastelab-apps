cab1dd1f267bf6ffb8ef0349311fdfd8eca44ba1
Ibnu Prayogi <ibnu.dev@gmail.com>
2020-12-20 19:20:44 +0700

    [002] adding aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          "root": ['./'],
+          "alias": {
+            '@assets': './assets',
+            '@components': './components',
+            '@hooks': './hooks',
+            '@routes': './routes',
+            '@screens': './screens',
+            '@storage': './storage'
+          }
+        }
+      ]
+    ]
   };
 };
